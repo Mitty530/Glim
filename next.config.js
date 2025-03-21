c260ff7a8cf87ca580e2a1ca2874868fee6239bd
Mitty <mamadouourydiallo819@gmail.com>
2025-03-21 16:46:28 +0400

    Initial commit - Landing page with waitlist functionality

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['images.unsplash.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
+  experimental: {
+    runtime: 'edge',
+    serverComponents: true,
+  },
+  webpack(config) {
+    // Support for GLSL shaders
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ['raw-loader', 'glslify-loader'],
+    })
+    
+    return config
+  },
+}
+
+module.exports = nextConfig 
